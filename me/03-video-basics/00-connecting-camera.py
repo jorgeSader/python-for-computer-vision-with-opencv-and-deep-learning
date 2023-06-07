085f48383972a6c869acd3dccecb06fc385bf2be
Jorge Sader <Jorge.Sader@gmail.com>
2023-06-07 07:27:27 -0400

    Completed 02.- Video Assesment

---
+++ me/03-video-basics/00-connecting-camera.py
@@ -0,0 +1,3 @@
+sudo dnf install cmake python-devel numpy gcc gcc-c++ gtk2-devel libdc1394-devel ffmpeg-devel libpng-devel libjpeg-turbo-devel jasper-devel openexr-devel libtiff-devel libwebp-devel tbb-devel eigen3-devel doxygen
+
+gstreamer-plugins-base-devel
